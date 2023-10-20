c13df11b50dc0a6f4d50dbddaf0cf82b435dc476
Basedfloppa <113017705+Basedfloppa@users.noreply.github.com>
2023-10-20 05:19:00 +0300

    Database added
    added base structure, probabaly will rework later for properly setting up fk

--- HealthBot_DB/Table1.sql
+++ HealthBot_DB/Table1.sql
@@ -1,4 +1,20 @@
-﻿CREATE TABLE [dbo].[Table1]
+﻿CREATE TABLE [dbo].[Users]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[UUID] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [Name] TEXT NULL, 
+    [Alias] TEXT NOT NULL, 
+    [Chat ID] BIGINT NOT NULL, 
+    [Age] INT NULL, 
+    [Sex] TEXT NULL ,
+    CHECK (SEX IN ('Male','Female')),
+    [Weight] INT NULL, 
+    [Doctor ID's] UNIQUEIDENTIFIER NULL, 
+    [Diary Entry's] UNIQUEIDENTIFIER NULL, 
+    [Subscription End] DATETIME NULL, 
+    [Subscription Start] DATETIME NULL, 
+    [Creation Date] DATETIME NOT NULL, 
+    [Change Date] DATETIME NOT NULL, 
+    [Destruction Date] DATETIME NOT NULL, 
+    FOREIGN KEY ([Doctor ID's]) REFERENCES [Users]([UUID]),
+    FOREIGN KEY ([Diary Entry's]) REFERENCES [Diary Entry's]([UUID])
 )
---
+++ HealthBot_DB/Table1_1.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Diary Entry's]
+(
+	[UUID] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [Data] DATETIME NOT NULL, 
+    [Intake Item's] UNIQUEIDENTIFIER NULL, 
+    [Author] UNIQUEIDENTIFIER NOT NULL, 
+    [Type] TEXT NOT NULL, 
+    [Heart Rate] INT NULL, 
+    [Blood o2 saturation] INT NULL, 
+    [Blood Preassute] TEXT NULL, 
+    [Creation Date] DATETIME NOT NULL, 
+    [Change Date] DATETIME NOT NULL, 
+    [Destruction Date] DATETIME NOT NULL,
+    FOREIGN KEY ([Author]) REFERENCES [Users]([UUID]),
+    FOREIGN KEY ([Intake Item's]) REFERENCES [Intake Item's]([UUID])
+)
