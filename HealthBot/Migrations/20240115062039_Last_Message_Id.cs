d0e90f342fccbb25f94de6f9f1fa4334198b206d
Basedfloppa <113017705+Basedfloppa@users.noreply.github.com>
2024-01-15 10:04:33 +0300

    added message id to migrations

---
+++ HealthBot/Migrations/20240115062039_Last_Message_Id.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HealthBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class Last_Message_Id : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "messageid",
+                table: "users",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "messageid",
+                table: "users");
+        }
+    }
+}
